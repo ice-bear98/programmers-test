c07fc85cca458150a6716d21d75c25fc2dd533d5
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2023-12-27 13:53:32 +0900

    [level unrated] Title: 짝수의 합, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let arr = [];
+    for(let i = 0; i <= n ; i++) {
+        if(i % 2 === 0) {
+            arr.push(i);
+        }
+    }
+    
+    return arr.reduce((acc,cur) => {return acc + cur} , 0)
+}
